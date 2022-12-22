6610ef6191ae44a2da59a3b167627f07b92a5224
DmitriySer <111710614+DmitriySer@users.noreply.github.com>
2022-12-22 05:14:00 +0400

    v2

--- index.php
+++ index.php
@@ -6,52 +6,77 @@ class TestParser
     public $file;
     public $selection;
     public $assignment;
+    public $read;
 
     function __construct($link, $selection, $assignment)
     {
+        set_time_limit(0);
         $this->link = $link;
         $this->selection = $selection;
         $this->assignment = $assignment;
     }
 
-    public function parser()
+    public function clearLink()
     {
-        preg_match('#\/\/(.*?)\/#',$this->link,$name);
+        preg_match('#\/\/(.*?)\/#', $this->link, $name);
         $this->file = $name[1];
-        if(!file_exists($this->file.'.html')){
-        $file = fopen($this->file.'.html','w');
-        $timeStart = microtime(true);
-        $html = $this->link;
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_URL, $html);
-        curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
-        curl_setopt($ch, CURLOPT_FILE, $file);
-        curl_exec($ch);
-        curl_close($ch);
-        $timeEnd = microtime(true);
-        $time = $timeEnd - $timeStart;
-        if ($time > 10){
-            echo "Дительность более 10 секунд";
-            exit();
-        }else {
-            echo "Длительность загрузки " . round($time, 2) . " сек ";
-        }
-        }
+
         return $this->file;
     }
 
+    public function parser()
+    {
+        $this->clearLink();
+        $html = $this->link;
+        $ch = curl_init($html);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+        $response = curl_exec($ch);
+        if (!$response){
+            echo "Ошибка подключения к адресу " . $this->link;
+            exit();
+        }else {
+            $file = fopen($this->file . '.html', 'w');
+            curl_setopt($ch, CURLOPT_FILE, $file);
+            curl_exec($ch);
+        }
+        curl_close($ch);
+    }
+
+
     public function replacement()
     {
-        $this->parser();
         $read = file_get_contents($this->file.'.html');
         $read = str_replace( $this->selection, $this->assignment, $read);
         file_put_contents($this->file.'.html', $read);
+
         echo $read;
+    }
+
+    public function start()
+    {
+        $time_start = microtime(true);
+        $this->clearLink();
+        try {
+            if (!file_exists($this->file . '.html'))
+            {
+                throw new Exception();
+            }
+            $this->replacement();
+        }catch (Exception $a)
+        {
+            $this->parser();
+            $this->replacement();
+        }
+        $time_end = microtime(true);
+        $time = $time_end - $time_start;
+        echo 'Времени заняло ' . $time;
     }
 
 }
 $a = new TestParser($argv[1],$argv[2],$argv[3]);
-print_r($a->replacement());
+print_r($a->start());
 
 
